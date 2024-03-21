22388739003394e9dc573d9a1dd305bdc37b57be
matoko <matheuslisboabr@gmail.com>
2024-03-20 23:41:11 -0300

    Update main.py
    requisitos e esboço inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+### O SISTEMA TEM QUE CRIA/EXCLUIR(TALVEZ UM CRUD) O ENTREVISTADO
+### O SISTEMA TEM QUE ARMAZENAR DADOS DAS 5 ENTREVISTA PARA CADA ENTREVISTADO (DICIONARIO)
+### O SISTEMA TEM QUE BUSCAR OS ENTREVISTADOS QUE TEM A NOTA IQUAL OU MAIOR PARA VAGA NOTA PARA VAGA PESQUESADA.
+### MENU DE ESCOLHA
+
+listaDeEntrevistados = []
+entrevistado = {"nome" : "","e": 0,"t": 0,"p": 0,"s": 0}
+
+
+def addEntrevistado():
+    pass
+
+def excluirEntrevistado():
+    pass
+
+def pesquisarEntrevistado():
+    pass
+
