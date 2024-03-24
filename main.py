e4592858878a6b56d33087ea766ad4f4083e5fd1
matoko <matheuslisboabr@gmail.com>
2024-03-24 20:22:31 -0300

    Update main.py
    add def excluirCandidato

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 ### O SISTEMA TEM QUE BUSCAR OS candidatoS QUE TEM A NOTA IQUAL OU MAIOR PARA VAGA NOTA PARA VAGA PESQUESADA.
 ### MENU DE ESCOLHA
 
-listaDeCandidatos = []
+# lista que ira receber os candidatos
+listaDeCandidatos = [{'nome': 'ana', 'resultado': {'e': 10, 't': 10, 'p': 10, 's': 10}}, {'nome': 'joao', 'resultado': {'e': 2, 't': 5, 'p': 10, 's': 0}}, {'nome': 'jana', 'resultado': {'e': 0, 't': 0, 'p': 0, 's': 10}}] #[]
 
 
 def addCandidato():
@@ -11,18 +12,37 @@ def addCandidato():
 # loop entre chaves e valores, usando o método:items()
     for x,y in candidato.items():
             if x == "nome":
-                #pega nome do candidato 
-                candidato[x] = input(f" {x} :")
+                # pega nome do candidato 
+                candidato[x] = input(f" {x} :").lower()
+                # chave resultado acresentada automaticamente
                 candidato["resultado"] = {"e": 0,"t": 0,"p": 0,"s": 0}
+
+                # talvez add def verificar nome ja exitente:
             else:
                 for z in y:
-                    candidato[x][z] =  int(input(f" {z} :"))         
+                    candidato[x][z] =  int(input(f" {z} :")) 
+                # talvez add def verificar valores intevalo de 0 a 10:       
             pass
-    listaDeCandidatos.append(candidato)          
+    listaDeCandidatos.append(candidato)
+    print("Candidato adicionado")        
 pass
 
 
+
 def excluirCandidato():
+    global listaDeCandidatos
+
+    nome = input("digite o nome do candidato a ser excluido :").lower()
+
+    for candidatos in listaDeCandidatos:
+        if candidatos["nome"] == nome: 
+        # criacao de nova lista interando sobre a antiga sem o nome do usuario removido
+            listaDeCandidatos = [candidato for candidato in listaDeCandidatos if candidato["nome"] != nome]
+            print("O Canditato foi excluido.")
+            break
+        else:
+            print("nao ha canditado com esse nome")
+            break 
     pass
 
 def pesquisarCandidato():
@@ -31,7 +51,4 @@ def pesquisarCandidato():
 def verLista():
     pass
 
-
-addCandidato()
 print(listaDeCandidatos)
-
