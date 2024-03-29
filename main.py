421750d84fa101361d2fc6ddeeb0232ac235c8e6
matoko <matheuslisboabr@gmail.com>
2024-03-29 15:00:25 -0300

    converte def  pesquisarCandidatoIdeal

--- main.py
+++ main.py
@@ -4,32 +4,23 @@
 ### MENU DE ESCOLHA
 
 # lista que ira receber os candidatos
-listaDeCandidatos = [] #[]
+listaDeCandidatos = [[],['ana', [1, 10, 10, 10]]] #[]
 nomesTestes = ["entrevista","teste teórico","teste prático","soft skills"]
 
 def addCandidato():
-   
     resultado = []
     nome = input(f"""Digite o nome do Candidato em: """).lower()
     for x in nomesTestes:
-        nota = input(f"""Digite a nota em {x}: """).lower()
+        nota = int(input(f"""Digite a nota em {x}: """))
         resultado.append(nota)
     candidato = [nome] + [resultado]
     listaDeCandidatos.append(candidato)
+
 
 # addCandidato()
 # print(listaDeCandidatos)
 
 def excluirCandidato():
-    # global listaDeCandidatos
-    # nome = input("digite o nome do candidato a ser excluido :").lower()
-    # for candidatos in listaDeCandidatos:
-    #     if candidatos["nome"] == nome: 
-    #     # criacao de nova lista interando sobre a antiga, sem o nome do usuario removido.
-    #         listaDeCandidatos = [candidato for candidato in listaDeCandidatos if candidato["nome"] != nome]
-    #         print("O Canditato foi excluido.")
-    #     else:
-    #         print("nao ha canditado com esse nome")
     nome = input("digite o nome do candidato a ser excluido :").lower()
     for candidato in listaDeCandidatos:
         if nome in candidato:
@@ -38,46 +29,46 @@ def excluirCandidato():
             candidato.pop(indece)
 
 # excluirCandidato()
-print(listaDeCandidatos)
+# print(listaDeCandidatos)
 
 def pesquisarCandidatoIdeal():
-    listaDeAprovados = []
-    # print(listaDeCandidatos)
-    notas = [0,1,2,3] ### talves coloca um dicionario seja a melhor opcao(a lista muda de valor imposbilitando a chama a def novamente...)
-    # print(notas[3])
-    for nota in notas:
-        notas[nota] = int(input(f"insira a notas: "))
-        # print(notas)
-    pass
-      
+    Aprovados = []
+    notasPesquisa = []
 
-    for candidatos in listaDeCandidatos:
-        # print(type(candidatos["resultado"]["e"]))
-        # print(notas)
-        condicao = candidatos["resultado"]["e"] >= notas[0] and candidatos["resultado"]["t"] >= notas[1] and candidatos["resultado"]["p"] >= notas[2] and candidatos["resultado"]["s"] >= notas[3]
+    for nota in nomesTestes:
+        nota = int(input(f"insira de {nota}: "))
+        notasPesquisa.append(nota)
 
-        if condicao:
-            print("aprovado")
-            print(candidatos)
-            listaDeAprovados.append(candidatos)
+    for canditato in listaDeCandidatos:
+        if canditato != []:
+            print(canditato)
+            notasCandidato = canditato[1]
+            print(canditato[1])
+            qualificado = True
+    
+    for notasCandidato,notasPesquisa in zip(notasCandidato,notasPesquisa):
+        if notasCandidato < notasPesquisa:
+            qualificado = False
+            print("off")
+            break
+    
+    if qualificado:
+        Aprovados.append(canditato)
 
-        else:
-            print("nao")
+    if Aprovados:
+        print(f"candidatos aprovados")
+        for candidatos in Aprovados:
+            print(canditato)
+    else:
+        print("ninguem passou :( ")
 
 
-    # for candidatos in listaDeCandidatos:
-    #     # print(candidatos["resultado"])
-    #     condicao = candidatos["resultado"]["e"] and >= candidatos["resultado"]["t"] >= and candidatos["resultado"]["p"] >= and candidatos["resultado"]["s"]:
-    #  for x in listaDeCandidatos:
-    #         if x == "resultado":
-    #               for z in y:
-    #                  if x[x]["e"] >= 5 and x[x]["t"] >= 5 and x[x]["p"] >= 5 and x[x]["s"] >= 5 :
-    #                     print("aprovado")
-    #                     print(x)
-    #                  else:
-    #                     print("nao")
-    #                     print(x)
-    #                  break
+        
+
+
+pesquisarCandidatoIdeal()
+
+
 pass
         
 
