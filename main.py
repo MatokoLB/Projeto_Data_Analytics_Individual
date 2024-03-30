4829f5a029e1adc953e5a1b1169b67d40382e227
matoko <matheuslisboabr@gmail.com>
2024-03-29 21:40:36 -0300

    add interface nav

--- main.py
+++ main.py
@@ -4,32 +4,38 @@
 ### MENU DE ESCOLHA
 
 # lista que ira receber os candidatos
-listaDeCandidatos = [[],['ana', [1, 10, 10, 10]]] #[]
+listaDeCandidatos = [[],[]] #[]
 nomesTestes = ["entrevista","teste teórico","teste prático","soft skills"]
 
 def addCandidato():
     resultado = []
-    nome = input(f"""Digite o nome do Candidato em: """).lower()
+    nome = input(f"""
+                 
+                 Digite o nome do Candidato em: 
+                 
+                 """).lower()
+
     for x in nomesTestes:
-        nota = int(input(f"""Digite a nota em {x}: """))
+        nota = int(input(f"""
+                         
+                        Digite a nota em {x}: 
+                         
+                         """))
         resultado.append(nota)
     candidato = [nome] + [resultado]
     listaDeCandidatos.append(candidato)
 
 
-# addCandidato()
-# print(listaDeCandidatos)
-
 def excluirCandidato():
-    nome = input("digite o nome do candidato a ser excluido :").lower()
+    nome = input("Digite o nome do candidato a ser excluido :").lower()
     for candidato in listaDeCandidatos:
         if nome in candidato:
             indece = candidato.index(nome)
             candidato.pop(indece + 1)
             candidato.pop(indece)
 
-# excluirCandidato()
-# print(listaDeCandidatos)
+listaDeCandidatos = [x for x in listaDeCandidatos if x != []]
+
 
 def pesquisarCandidatoIdeal():
     Aprovados = []
@@ -45,15 +51,16 @@ def pesquisarCandidatoIdeal():
             notasCandidato = canditato[1]
             print(canditato[1])
             qualificado = True
-    
+            
+            
     for notasCandidato,notasPesquisa in zip(notasCandidato,notasPesquisa):
         if notasCandidato < notasPesquisa:
             qualificado = False
             print("off")
             break
-    
+
     if qualificado:
-        Aprovados.append(canditato)
+        Aprovados.append(canditato)  
 
     if Aprovados:
         print(f"candidatos aprovados")
@@ -63,17 +70,43 @@ def pesquisarCandidatoIdeal():
         print("ninguem passou :( ")
 
 
-        
-
-
-pesquisarCandidatoIdeal()
-
-
-pass
-        
-
 def verLista():
     print(listaDeCandidatos)
 pass
 
-
+while True:
+    opcoes = input (F"""
+    ####################################### S I S T E M A ###########################################      
+                                    bem-vindo ao #COMPATIBILITY#                  
+                                        SELECIONE UMA OPÇÃO
+    #################################################################################################                
+                                        
+                    
+        [1]- ADICIONAR CANDIDATO    [2]- EXCLUIR CANDIDATO      [3]- APLICAR VAGA A CANDIDATO
+                    
+        
+        [4]- VER LISTA              [5]- OPÇÕES AUTOMAÇÃO       [6]- CRÉDITOS        
+                                                                                                  
+                                                                                           
+                                                                                           [E]-SAIR
+    #################################################################################################  
+                    """)
+    
+    if opcoes == "1":
+        addCandidato()
+        pass
+    elif opcoes == "2":
+        excluirCandidato()
+        pass
+    elif opcoes == "3":
+        pesquisarCandidatoIdeal()
+    elif opcoes == "4":
+        verLista()
+    elif opcoes == "5":
+       pass
+    elif opcoes == "6":
+        pass
+    elif opcoes == "7":
+        exit()
+    else:
+        pass
